6bf02b3371ab414388bd8ad6902a00f8c9a890d1
dineshtripathi30 <dinesh.tripathi30@gmail.com>
2022-04-08 18:56:47 +0530

    updated file

---
+++ src/main/java/com/example/tapdemo2/Tapdemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.tapdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tapdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tapdemo2Application.class, args);
+	}
+
+}
